bac22fe29c736d395ad963c157f11914cf9b6eea
Dmytro1001 <109743079+Dmytro1001@users.noreply.github.com>
2023-04-26 18:59:30 +0000

    Deploying to gh-pages from @ Dmytro1001/-tweet-cards-@d436597e1f779d6b14fe389372bb4f1bb0bf1ec8 🚀

--- static/js/911.b5b0e40e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_tweet_cards_=self.webpackChunk_tweet_cards_||[]).push([[911],{911:function(e,t,r){r.r(t);var s=r(184);t.default=function(){return(0,s.jsx)("p",{children:"Will be here Tweets page"})}}}]);
-//# sourceMappingURL=911.b5b0e40e.chunk.js.map
